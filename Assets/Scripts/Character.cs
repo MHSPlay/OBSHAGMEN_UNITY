4c9b3c229dbaa6c651b725d825f6fed81551475d
MHSPlay <41786546+MHSPlay@users.noreply.github.com>
2024-10-10 21:20:31 +1000

    Fix PROJECT

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[ RequireComponent( typeof( Rigidbody ) ) ]
+public class Character : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float mouseSensitivity = 1000f;
+    public float interactionDistance = 2f;
+    public Transform cameraHolder;
+
+    private float xRotation = 0f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        // lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        Move();
+        if (Input.GetKeyDown(KeyCode.E) && interactable != null)
+        {
+            interactable.onInteract.Invoke();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Interaction();
+    }
+
+    void LateUpdate()
+    {
+        RotateCamera();
+    }
+
+    // note: writen by MHSPlay
+    void Move()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal"); // (A/D)
+        float moveZ = Input.GetAxisRaw("Vertical");   // (W/S)
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        move.y = 0;
+        move.Normalize(); // fixed speed diagonal move
+
+        Vector3 velocity = move * moveSpeed;
+        rb.velocity = new Vector3(velocity.x, rb.velocity.y, velocity.z);
+    }
+
+    // note: writen by MHSPlay
+    // todo: fix camera jitter when moving player and mouse
+    void RotateCamera()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+
+        transform.localRotation *= Quaternion.Euler( 0f, mouseX, 0f);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -89f, 89f);
+
+        cameraHolder.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+    private InteractableObject interactable;
+
+    void Interaction()
+    {
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * interactionDistance);
+        if (Physics.Raycast(ray, out RaycastHit hit, interactionDistance))
+        {
+            if (hit.collider.gameObject.layer != 3)
+            {
+                return;
+            }
+
+            if (interactable == null)
+            {
+                interactable = hit.collider.GetComponent<InteractableObject>();
+                interactable.onCursorEnter.Invoke();
+            }
+            else
+            {
+                if (interactable.gameObject != hit.collider.gameObject)
+                {
+                    interactable = hit.collider.GetComponent<InteractableObject>();
+                    interactable.onCursorEnter.Invoke();
+                }
+            }
+            
+            
+            if(interactable.gameObject != hit.collider.gameObject)
+            {
+                interactable = hit.collider.GetComponent<InteractableObject>();
+                interactable.onCursorEnter.Invoke();
+            }
+        }
+        else
+        {
+            if(interactable != null)
+            {
+                interactable.onCursorExit.Invoke();
+                interactable = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+//By Kakins
+public class InteractableObject : MonoBehaviour
+{
+    [SerializeField] public UnityEvent onInteract;
+    [SerializeField] public UnityEvent onCursorEnter;
+    [SerializeField] public UnityEvent onCursorExit;
+
+    private void Start()
+    {
+        gameObject.layer = 3;
+    }
+}
